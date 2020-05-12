0c8b275876883a105f75af96b6acea819b555de8
ecoen66 <ecoen@yahoo.com>
2020-05-11 19:48:19 -0500

    touched index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,5 @@ import { RaritanHomebridgePlatform } from './platform';
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, RaritanHomebridgePlatform, true);
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, RaritanHomebridgePlatform);
 }
